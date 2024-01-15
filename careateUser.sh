3160a86d6f7f9262e137bb44827fa63eb05cd2f2
kumakura <kumakuma0401kuma@gmail.com>
2024-01-15 12:58:15 +0900

    add comment

--- careateUser.sh
+++ careateUser.sh
@@ -8,16 +8,13 @@ fi
 
 # if OS is debian or ubuntu
 if [ -f /etc/debian_version ]; then
-
+    #add user and add to sudo group
     sudo adduser $USER 
     sudo gpasswd -a $USER sudo
-
-    #cat /etc/passwd | grep $USER
-    #cat /etc/group | grep sudo 
-
+    
+    #check if user is added to sudo group
     RESULT=$(cat /etc/group | grep sudo)
     PATTERN=".*${1}"
-
     if [[ $RESULT =~ $PATTERN ]]; then
         echo "process succeed"
     else
@@ -25,12 +22,14 @@ if [ -f /etc/debian_version ]; then
     fi
 # if OS is centos or fedora
 elif [ -f /etc/redhat-release ]; then
-
+    
+    #add user and add to wheel group
     sudo adduser $USER_NAME
     sudo passwd $USER_NAME    
 
     sudo usermod -aG wheel $USER_NAME
 
+    #check if user is added to wheel group
     RESULT=$(cat /etc/group | grep wheel)
     PATTERN=".*${1}"
 
@@ -41,3 +40,4 @@ elif [ -f /etc/redhat-release ]; then
     fi
 
 fi
+
