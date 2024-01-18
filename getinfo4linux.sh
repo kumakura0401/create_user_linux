6feebca5419632825bb9fdb59497e3ab9aeff316
kumakura <kumakuma0401kuma@gmail.com>
2024-01-18 16:25:59 +0900

    modified getinfo4linux.sh and create .gitignore

--- getinfo4linux.sh
+++ getinfo4linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#if os distribution is debian or ubuntu
 if [ -f /etc/debian_version ]; then
     # get OS distribution
     OS=$(grep PRETTY_NAME /etc/os-release)
@@ -20,9 +21,31 @@ if [ -f /etc/debian_version ]; then
     USEDLANG=$LANG
 fi
 
+#if os distribution is centos or fedora
+if [ -f /etc/redhat-release ]; then
+    # get OS distribution
+    OS=$(cat /etc/redhat-release)
+    HOSTNAME=$(hostname)
+    domain=$(hostname -d)
+    IP=$(hostname -I)
+    MAC=$(ip link show | awk '/ether/ {print $2}')
+
+    # check ds_agent and crowdstrike
+    
+    DEEP_SEC=$(sudo systemctl status ds_agent | grep active) 
+    CROWDSTRIKE=$(sudo systemctl status falcon-sensor | grep active)
+
+    # check hardware
+    HARD=$(sudo dmidecode)
+
+    # check language
+    USEDLANG=$LANG
+fi
+
+
 touch $HOSTNAME.csv
 
-cat<<EOF>$HOSTNAME.csv
+cat<<EOF>$HOSTNAME.csvls
 "OS",$OS
 "Hostname",$HOSTNAME
 "Domain",$domain
@@ -33,3 +56,4 @@ cat<<EOF>$HOSTNAME.csv
 "Hardware",$HARD
 "Language",$USEDLANG
 EOF
+
